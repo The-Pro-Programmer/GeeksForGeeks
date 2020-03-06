8d34f2b7a84fdcc26a3d6959a5825f10d2b41cc2
Mansi Wakhankar <mansiswakharkar@gmail.com>
2020-03-06 14:25:57 +0530

    Count Leaves in Binary Tree

--- src/dataStructures/trees/TreeProblems.java
+++ src/dataStructures/trees/TreeProblems.java
@@ -1,7 +1,49 @@
 package dataStructures.trees;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TreeProblems {
 
+	
+	// Count Leaves in Binary Tree - Iterative
+	int countLeavesIterative(Node node) {
+    	if(node==null) {
+    		return 0;
+    	}
+    	Queue<Node> queue = new LinkedList<>();
+    	queue.add(node);
+    	int count = 0;
+    	while(!queue.isEmpty()) {
+    		Node temp = queue.poll();
+    		if(temp!=null) {
+    			if(temp.left==null && temp.right==null) {
+    				count++;
+    			}else {
+    				if(temp.left!=null) {
+    					queue.add(temp.left);
+    				}
+    				if(temp.right!=null) {
+    					queue.add(temp.right);
+    				}
+    			}
+    		}
+    	}
+    	return count;
+    }
+	
+	// Count Leaves in Binary Tree - Recursive
+	int countLeavesRecursive(Node node) {
+    	if(node==null){
+    		return 0;
+    	} 
+    	if(node.left==null && node.right==null){
+    		return 1;
+        }
+        return countLeavesRecursive(node.left) + countLeavesRecursive(node.right);
+    }
+	
 	boolean isIdentical(Node root1, Node root2) {
 		String path1 = inorder(root1);
 		String path2 = inorder(root2);
@@ -15,17 +57,16 @@ public class TreeProblems {
 		return inorder(root.left) + root.data + inorder(root.right);
 	}
 
-	int height(Node node)  {
 	
+	//Height of Binary Tree
+	int height(Node node)  {	
 		if(node==null){
 			return 0;
-		}
-	
+		}	
 		int left = height(node.left);
 		int right = height(node.right);
 		int max = Integer.max(left, right);
 		return max+1;
-
      }
 
 }
