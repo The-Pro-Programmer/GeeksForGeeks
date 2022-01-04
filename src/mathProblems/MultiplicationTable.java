7373a911fc77849efdf4156f991f3936d4b650ec
Mansi Wakhankar <mansiswakharkar@gmail.com>
2022-01-04 17:25:50 +0300

    Create multiplication table

---
+++ src/mathProblems/MultiplicationTable.java
@@ -0,0 +1,8 @@
+
+    static ArrayList<Integer> getTable(int N){
+        ArrayList<Integer> result = new ArrayList<>();
+        for(int i=1; i<=10; i++){
+            result.add(i*N);
+        }
+        return result;
+    }
